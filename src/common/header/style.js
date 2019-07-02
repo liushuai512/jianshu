836614059faa79dac54e2d90641da912ecdc4019
iverson0323 <kongge_3@163.com>
2019-07-02 17:22:04 +0800

    修改导航头，更新字体库

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -2,9 +2,16 @@ import styled from 'styled-components';
 import logoPic from '../../statics/logo.png'
 
 export const HeaderWrapper = styled.div`
+  
+  border-bottom: 1px solid #f0f0f0;
+`
+
+export const HeaderNav = styled.div`
+  max-width: 1440px;
+  min-width: 768px;
+  margin: 0 auto;
   position: relative;
   height: 56px;
-  border-bottom: 1px solid #f0f0f0;
 `
 
 export const Logo = styled.a.attrs({href: '/'})`
