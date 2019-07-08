cc7ef304966787f13aa9a0a45696c5122178396a
iverson0323 <kongge_3@163.com>
2019-07-08 13:46:07 +0800

    字体库

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -62,7 +62,7 @@ class Header extends Component {
           <IconfontStyle/>
           <Logo/>
           <Nav>
-            <NavItem className='left active'>首页</NavItem>
+            <NavItem className='left active'><i className='iconfont'>&#xe67b;</i>首页</NavItem>
             <NavItem className='left'>下载App</NavItem>
             <NavItem className='right'>登陆</NavItem>   
             <NavItem className='right'>
